32479f9fe9031e77da0956391ff7ed0bc1a55b95
ItisNoMatter <702sunnylife@gmail.com>
2022-08-08 22:01:46 +0900

    visualize対応

--- thread-test.py
+++ thread-test.py
@@ -1,10 +1,15 @@
+from os import system
 import sys
 from metronome import Metronome
 from midi_input import MidiIn,Message
+from visualize import StdPrint
 
 if __name__ == "__main__":
     midiin = MidiIn()# pylanceの警告がでないぜ！！！！！！！
     port = midiin.open_port()
+    metronome = Metronome(bpm=60)
+    metronome.atatch_visualizer(StdPrint())
+    metronome.start() 
     with port:
         try:
             while True:
@@ -12,5 +17,6 @@ if __name__ == "__main__":
                 if data:
                     m = Message(data)
                     print("status:{} note_num:{} velocity:{}".format(m.status,m.note_num,m.velocity))
+                    
         except KeyboardInterrupt:
             sys.exit()
