b8dd4892c73c87b4cee2be1cc19ea6935346f80c
ItisNoMatter <702sunnylife@gmail.com>
2022-08-08 21:20:30 +0900

    visualize対応

--- metronome.py
+++ metronome.py
@@ -2,30 +2,36 @@ import time
 import threading
 import sys
 
+from visualize import StdPrint, Visualizer
+
 class Metronome(threading.Thread):
-    def __init__(self,bpm:int=60,timesig:int=4,ppq=4) -> None:
+    def __init__(self,bpm:int=60,timesig:int=4,ppq:int=4) -> None:
         super(Metronome,self).__init__()
         self.daemon = True
-
         #immutable(にしたい)
         self.bpm:int = bpm
         self.timesig:int = timesig
         self._ppq:int = ppq #Pulses per quarter note 
-        
         #mutable
         self.current_tick:int = 1
         self.current_beat:int = 1
         self.currrent_bar:int = 1
-
         #debug
         self.last_call_time:float = time.time()
         self.interval:float = 0
-        pass
+
+    def atatch_visualizer(self,v:Visualizer):
+        self.visualizer = v
     def run(self):
         print(self.timesig)
         while True:
             time.sleep((self.bpm/60)/self._ppq)
             self._increment_tick()
+            try:
+                if self.visualizer:
+                    self.visualizer.print_beat(self.currrent_bar,self.current_beat)
+            except AttributeError:
+                pass
 
     def _increment_tick(self):
         if self.current_tick == self._ppq:
@@ -47,15 +53,18 @@ class Metronome(threading.Thread):
         pass
     def pause(self):
         pass
+
 if __name__ == "__main__":
     m = Metronome(bpm=60,timesig=4)
     m.start()
+    a = StdPrint()
+    m.atatch_visualizer(StdPrint())
     try:
         while True:
             time.sleep(0.01)
             #print("\r"+ " beat:"str(m.current_beat),end="")
-            print("\rbar: {} beat: {} tick: {:03}".format(m.currrent_bar,m.current_beat,m.current_tick))
-            print("\rerror_time(s/beat): {} ".format(m.interval-1) +"\033[2A",end="")
-            print()
+            #print("\rbar: {} beat: {} tick: {:03}".format(m.currrent_bar,m.current_beat,m.current_tick))
+            #print("\rerror_time(s/beat): {} ".format(m.interval-1) +"\033[2A",end="")
+            #print()
     except KeyboardInterrupt:
         sys.exit()
