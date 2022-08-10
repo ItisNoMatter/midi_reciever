9adce7eb4da0ee04c4b7cd64dda95cddbdbf8f41
ItisNoMatter <702sunnylife@gmail.com>
2022-08-10 23:22:44 +0900

    Scoreオブジェクトのテストを書いた

--- Score.py
+++ Score.py
@@ -12,3 +12,8 @@ class Note:
 class Score:
     def __init__(self) -> None:
         self.notes:List[Note] = []
+    def __str__(self):
+        s = ["note:{:4} time:{}".format(n.note_num,n.beat.time,) for n in self.notes]
+        return '\n'.join(s)
+    def add_note(self,note:Note):#この機能だけだと属性に直接アクセスしたほうが良いが、あとでいろいろ機能追加しそうなのでこうしている
+        self.notes.append(note)
--- thread-test.py
+++ thread-test.py
@@ -1,5 +1,6 @@
 from os import system
 import sys
+from Score import Note, Score
 from metronome import Metronome
 from midi_input import MidiIn,Message
 from visualize import StdPrint
@@ -9,6 +10,7 @@ if __name__ == "__main__":
     port = midiin.open_port()
     metronome = Metronome(bpm=60)
     metronome.atatch_visualizer(StdPrint())
+    score = Score()
     metronome.start() 
     with port:
         try:
@@ -16,7 +18,10 @@ if __name__ == "__main__":
                 data = port.get_message()
                 if data:
                     m = Message(data)
+                    note = Note(metronome.get_beat_time(),m.note_num)
+                    score.add_note(note)
                     print("status:{} note_num:{} velocity:{}".format(m.status,m.note_num,m.velocity))
                     
         except KeyboardInterrupt:
+            print(str(score))
             sys.exit()
